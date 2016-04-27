6487a08f32d7e9ce3731e1f69b6b1e6b413a1b65
Captainelaine <zxf76@miami.edu>
2016-04-26 23:52:41 -0400

    finish multi

--- js/main.js
+++ js/main.js
@@ -14,12 +14,17 @@
   resize("ms2");
   resize("ms3");
   resize("page4");
+  resize("page5");
+  resize("homepage");
+
 
   window.onresize = function() {
     resize("ms1");
     resize("ms2");
     resize("ms3");
     resize("page4");
+    resize("page5");
+    resize("homepage");
   };
 
 //global variable
@@ -33,14 +38,15 @@ queue()
     .defer(d3.csv, "data/mapdata.csv", typestyle)
     .defer(d3.csv, "data/World_Development_Indicators_Metadata_Countries.csv", makeLookup)
     .defer(d3.json, "data/countries.json")
+    .defer(d3.csv, "data/ISOmulti.csv", typeFix)
     .await(loadedData);
 
-function loadedData(error,linechart,barchart,map1,map2,map3) {
+function loadedData(error,linechart,barchart,map1,map2,map3,mapline) {
 
   console.log(error);
   drawlinechart1(linechart);
   drawbarchart1(barchart);
-  drawmapchart(map1,map2,map3);
+  drawmapchart(map1,map2,map3,mapline);
 
 
 }
@@ -55,4 +61,34 @@ function typestyle(d) {
 function makeLookup(d) {
     countryById.set(d.ISO3, d);
     return d;
+}
+
+function typeFix(d) {
+  d.AUS = +d.AUS;
+  d.MYS = +d.MYS;
+  d.CHN = +d.CHN;
+  d.ESP = +d.ESP;
+  d.MCO = +d.MCO;
+  d.CAN = +d.CAN;
+  d.GBR = +d.GBR;
+  d.HUN = +d.HUN;
+  d.BEL = +d.BEL;
+  d.ITA = +d.ITA;
+  d.SGP = +d.SGP;
+  d.JPN = +d.JPN;
+  d.BRA = +d.BRA;
+  d.ARE = +d.ARE;
+  d.BHR = +d.BHR;
+  d.USA = +d.USA;
+  d.FRA = +d.FRA;
+  d.DEU = +d.DEU;
+  d.TUR = +d.TUR;
+  d.SMR = +d.SMR;
+  d.AUT = +d.AUT;
+  d.KOR = +d.KOR;
+  d.IND = +d.IND;
+  d.RUS = +d.RUS;
+  d.MEX = +d.MEX;
+
+  return d;
 }
